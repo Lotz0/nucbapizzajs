5df132440ac52e32899c81adf29ee8769e15f295
unknown <juanguastavino97@gmail.com>
2022-11-29 03:52:33 -0300

    localstorage

--- scripts.js
+++ scripts.js
@@ -1,23 +1,12 @@
-
-
-class Pizzas {
-    constructor( nombre, id, precio, ingredientes){
-        this.nombre = nombre;
-        this.id = id;
-        this.precio = precio;
-        this.ingredientes = ingredientes;
-    }
-}
-
 const pizza = [
-   new Pizzas ("muzzarella", 1,  500,  ["muzzarella", "oregano"]),
-   new Pizzas("napolitana", 2,  780,  ["muzzarella", "oregano", "tomate"]),
-   new Pizzas("palmitos", 3,  750,  ["muzzarella", "oregano", "palmitos", "salsa golf"]),
-   new Pizzas ("americana", 4,  900,  ["muzzarella", "calabresa"]),
-   new Pizzas("4 quesos", 5,  1200,  ["muzzarella", "oregano", "parmesano", "fontina", "provolone"]),
-   new Pizzas ("jamon y morrones", 6,  800,  ["muzzarella", "jamon", "morron"]),
-   new Pizzas("crudo y rucula", 7,  1600,  ["muzzarella", "jamon crudo", "rucula"]),
-   new Pizzas("champinones", 8,  1000,  ["muzzarella", "champinones", "salsa blanca"]),
+    {nombre: "muzzarella", id: 1,  precio:500,  ingredientes: ["muzzarella", "oregano"], imagen: "jamon-morron-frog.png"} ,
+    {nombre:"napolitana", id:2,  precio:780,  ingredientes:["muzzarella", "oregano", "tomate"], imagen:"pizza1.png"},
+    {nombre:"palmitos", id:3,  precio:750,  ingredientes:["muzzarella", "oregano", "palmitos", "salsa golf"], imagen:"Pizza-Jamon-con-Palmitos.png"},
+    {nombre:"americana", id:4,  precio:900,  ingredientes:["muzzarella", "calabresa"], imagen:"pizza2.png"},
+    {nombre:"4 quesos", id:5,  precio:1200,  ingredientes:["muzzarella", "oregano", "parmesano", "fontina", "provolone"], imagen:"pizza3.png"},
+    {nombre:"jamon y morrones", id:6,  precio:800,  ingredientes:["muzzarella", "jamon", "morron"], imagen:"pizza4.png"},
+    {nombre:"crudo y rucula", id:7,  precio:1600,  ingredientes:["muzzarella", "jamon crudo", "rucula"], imagen:"porcionmargarita.png"},
+    {nombre:"champinones", id:8,  precio:1000,  ingredientes:["muzzarella", "champinones", "salsa blanca"], imagen:"pizza-cuatro-quesos-gourmet-aislado_219193-8117.webp"},
 ]
 
 const form = document.getElementById('formPizza');
@@ -26,6 +15,7 @@ const showPizza = document.getElementById('pizzaName');
 const ShowPrice = document.getElementById('pizzaPrice');
 const error = document.getElementById("failure");
 const pizzaContainer = document.getElementById("resultados");
+const divImagenPizza = document.getElementById('pizzaImg');
 
 const nombrePizza = (pizzaNombre) => {
     showPizza.textContent = `nombre de pizza:  ${pizzaNombre}`;
@@ -34,8 +24,10 @@ const nombrePizza = (pizzaNombre) => {
 const precioPizza = (pizzaPrecio) => {
     ShowPrice.textContent = ` precio: ${pizzaPrecio} `;
 };
+
+
 const EError = (id) => {
-    error.textContent = ` El valor ${id} es erroneo`;
+    error.textContent = ` El ID ${id} es erroneo`;
     pizzaContainer.style.display = 'none';
 };
 
@@ -43,19 +35,39 @@ const EError = (id) => {
 
 const buscador = () => {
     const pizza_id = pizza.find((pizza) => pizza.id == ID.value);
+    
     if (pizza_id) {
+
         pizzaContainer.style.display = 'flex';
         error.style.display = "none";
         nombrePizza(pizza_id.nombre);
         precioPizza(pizza_id.precio);
+        divImagenPizza.innerHTML = `
+        <div id="imagecont">
+            <img src="images/${pizza_id.imagen}" id:"imagenpizza">
+        </div>
+
+        `
+        
+        let pizzaJSON = JSON.stringify(pizza_id);
+        localStorage.setItem("pizzaData", pizzaJSON)
+        
     } else {
         EError(ID.value);
             error.style.display = "flex";
     }
-};
+}
+
+
 
 form.addEventListener('submit', (e) => {
     e.preventDefault();
     buscador();
     form.reset(); 
 });
+
+document.addEventListener("DOMContentLoaded", function(event){
+    let pizzaGuardada = JSON.parse(localStorage.getItem("pizzaData"))
+    console.log(pizzaGuardada)
+})
+
